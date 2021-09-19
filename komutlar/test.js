998f0ad3cf227d46a7dc93d93afc19df170af0bd
earslaner2001 <50261757+earslaner2001@users.noreply.github.com>
2021-09-20 01:48:15 +0300

    Add files via upload

---
+++ komutlar/test.js
@@ -0,0 +1,17 @@
+exports.run = function(client, message, args) {
+        message.channel.send("Discord Sunucumuz: https://discord.gg/Ya7X3PHk");
+    };
+
+module.exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["tst"],
+  permLevel: 0
+};
+
+module.exports.help = {
+  name: 'test',
+  description: '',
+  usage: 'test'
+};
+//XiR
